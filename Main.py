674a144dcb5fac0d8decf45b4c7b9860a6bec07e
saa19 <saaa1997@gmail.com>
2019-03-29 08:12:48 +0700

    dobroe ytro

--- Main.py
+++ Main.py
@@ -1,57 +1,105 @@
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import numpy as np
-from math import exp, sin, cos, pi
-
-m = 10
-hx = 1/m
-hy = 1/m
-
-nx = int(1/hx)+1
-ny = int(1/hy)+1
-
-u = np.zeros((ny, nx))
-f = np.zeros((ny, nx))
-ru = np.zeros((ny, nx))
-
-for i in range(0, nx):
-    for j in range(0, ny):
-        ru[j][i] = exp(i*hx)*cos(j*hy)
-
-for i in range(0, nx):
-    for j in range(0, ny):
-        f[j][i] = 0.2*exp(i*hx)*cos(j*hy)
+import copy
+import math
 
 
+def funca(s):
+    n = copy.deepcopy(s)
+    for j in range(1, N-1):
+        for i in range(1, j):
+            n[j][i] = (-a*(s[j][i-1]-2*s[j][i]+s[j][i+1])-b*(s[j-1][i]-2*s[j][i]+s[j+1][i]))/(h**2)
+    return n
 
 
+def funcb(s,l):
+    n = copy.deepcopy(s)
+    for j in range(1, N-1):
+        for i in range(1, j):
+            n[j][i] = l*s[j][i]-(-a*(s[j][i-1]-2*s[j][i]+s[j][i+1])-b*(s[j-1][i]-2*s[j][i]+s[j+1][i]))/(h**2)
+    return n
 
-xx = np.arange(0, 1+hx, hx)
 
-yy = np.arange(0, 1+hy, hy)
+def prod(s1, s2):
+    prsum = 0
+    for j in range(N-1):
+        for i in range(j+1):
+            prsum += s1[j][i] * s2[j][i]
+    return prsum
 
 
-for i in range(0, 11):
-    plt.figure(figsize=(20, 10))
-    plt.subplot(1, 1,  1)
-    plt.plot(xx, u[i * int(0.1 / hy)], 'bo')
-    plt.title('y = ' + str(i/10))
-    plt.xlabel('x')
-    plt.ylabel('Value')
-    plt.grid(True)
-    plt.subplot(1, 1,  1)
-    plt.plot(xx, ru[i * int(0.1 / hy)], 'r+')
-    plt.grid(True)
-    plt.show()
+def norm(s):
+    return math.sqrt(prod(s,s))
 
-plt.figure(figsize=(20, 10))
-plt.subplot(1, 1,  1)
-plt.plot(yy, u[:, int(0.5 / hx)], 'bo')
-plt.title('x = 0.5')
-plt.xlabel('y')
-plt.ylabel('Value')
-plt.grid(True)
-plt.subplot(1, 1,  1)
-plt.plot(yy, ru[:, int(0.5 / hx)], 'r+')
-plt.grid(True)
-plt.show()
+
+def normal(s):
+    n = copy.deepcopy(s)
+    normn = norm(n)
+    for j in range(1, N-1):
+        for i in range(1, j):
+            n[j][i] = n[j][i]/normn
+    return n
+
+
+N = 5
+h = 1/N
+a = 1
+b = 1.2
+S = [[np.sign(i*(j-i)*(N-j-1)) for i in range(j+1)] for j in range(N)]
+
+print(S, prod(funcb(S,1), S))
+
+
+# m = 10
+# hx = 1/m
+# hy = 1/m
+#
+# nx = int(1/hx)+1
+# ny = int(1/hy)+1
+#
+# u = np.zeros((ny, nx))
+# f = np.zeros((ny, nx))
+# ru = np.zeros((ny, nx))
+#
+# for i in range(0, nx):
+#     for j in range(0, ny):
+#         ru[j][i] = exp(i*hx)*cos(j*hy)
+#
+# for i in range(0, nx):
+#     for j in range(0, ny):
+#         f[j][i] = 0.2*exp(i*hx)*cos(j*hy)
+#
+#
+#
+#
+#
+# xx = np.arange(0, 1+hx, hx)
+#
+# yy = np.arange(0, 1+hy, hy)
+#
+#
+# for i in range(0, 11):
+#     plt.figure(figsize=(20, 10))
+#     plt.subplot(1, 1,  1)
+#     plt.plot(xx, u[i * int(0.1 / hy)], 'bo')
+#     plt.title('y = ' + str(i/10))
+#     plt.xlabel('x')
+#     plt.ylabel('Value')
+#     plt.grid(True)
+#     plt.subplot(1, 1,  1)
+#     plt.plot(xx, ru[i * int(0.1 / hy)], 'r+')
+#     plt.grid(True)
+#     plt.show()
+#
+# plt.figure(figsize=(20, 10))
+# plt.subplot(1, 1,  1)
+# plt.plot(yy, u[:, int(0.5 / hx)], 'bo')
+# plt.title('x = 0.5')
+# plt.xlabel('y')
+# plt.ylabel('Value')
+# plt.grid(True)
+# plt.subplot(1, 1,  1)
+# plt.plot(yy, ru[:, int(0.5 / hx)], 'r+')
+# plt.grid(True)
+# plt.show()
 
